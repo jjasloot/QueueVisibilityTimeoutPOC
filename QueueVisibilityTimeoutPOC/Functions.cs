b498eb8965d7b7cd9e27b2af5c841972ae947d51
Jaap Slootbeek <j.slootbeek@recognize.nl>
2019-03-28 12:24:33 +0100

    Commit

---
+++ QueueVisibilityTimeoutPOC/Functions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace QueueVisibilityTimeoutPOC
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, DateTimeOffset NextVisibleTime, ILogger logger)
+        {
+            logger.LogInformation(message);
+            logger.LogInformation("Now:" + DateTime.UtcNow + " NextVisibleTime: " + NextVisibleTime.ToString());
+        }
+    }
+}
--- QueueVisibilityTimeoutPOC/Program.cs
+++ QueueVisibilityTimeoutPOC/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace QueueVisibilityTimeoutPOC
 {
@@ -6,7 +10,30 @@ namespace QueueVisibilityTimeoutPOC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage(a => a.VisibilityTimeout = TimeSpan.FromSeconds(30));
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+
+            builder.ConfigureServices((context, services) =>
+            {
+                services.Configure<QueuesOptions>(options =>
+                {
+                    options.MaxPollingInterval = TimeSpan.FromSeconds(2);
+                    options.VisibilityTimeout = TimeSpan.FromSeconds(40);
+                });
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
         }
     }
 }
